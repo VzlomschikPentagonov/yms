3a7496900b8a5541fb27a7f18deffac48f77285d
VzlomschikPentagonov <5959jhihguoygbio54@gmail.com>
2025-09-30 15:25:14 +0700

    Update functions.py

--- src/functions.py
+++ src/functions.py
@@ -21,8 +21,9 @@ def get_header(header: Header) -> bytes:
                       c_uint16, c_uint32, c_uint32, c_uint8] = (HEADER_SIZE,
                       header.num_tracks, header.tempo, header.spb,
                       header.p_len, header.length, header.freq, header.bits)
-    bytestr : bytes = b""
+    bytestr: bytes = b""
     for var in header_data:
         bytestr += bytes(var)
     return bytestr
+
 
