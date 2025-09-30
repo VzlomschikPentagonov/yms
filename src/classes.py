061386bfece9620b4a6753875c9984ef2af5667d
VzlomschikPentagonov <5959jhihguoygbio54@gmail.com>
2025-09-30 15:04:40 +0700

    Update classes.py

--- src/classes.py
+++ src/classes.py
@@ -1,16 +1,19 @@
 from constants import *
 
 class Header:
-    def __init__(self,
-                 name: str = "UnnamedProject",
-                 tempo: c_float = 120.0,
-                 spb: c_uint16 = 60,
-                 p_len: c_uint16 = 240,
-                 freq: c_uint32 = 44100,
-                 bits: c_uint8 = 24):
+    def __init__(self, name: str = "UnnamedProject",
+                 tempo: c_float = TEMPO_DEFAULT,
+                 length: c_float = LENGTH_DEFAULT,
+                 freq: c_uint32 = FREQ_DEFAULT,
+                 spb: c_uint16 = SPB_DEFAULT,
+                 p_len: c_uint16 = P_LEN_DEFAULT,
+                 num_tracks: c_uint8 = NUM_TRACKS_DEFAULT,
+                 bits: c_uint8 = BITS_DEFAULT):
         self.name = name
         self.tempo = tempo
+        self.length = length
+        self.freq = freq
         self.spb = spb
         self.p_len = p_len
-        self.freq = freq
+        self.num_tracks = num_tracks
         self.bits = bits
