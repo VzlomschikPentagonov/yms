afe58e652acdf3a0132c912a3bbca7de6e0ab11a
VzlomschikPentagonov <5959jhihguoygbio54@gmail.com>
2025-10-07 16:42:23 +0700

    Update func_extra.py

--- src/func_extra.py
+++ src/func_extra.py
@@ -1,1 +1,10 @@
+from classes import *
 
+def generate_tones() -> list[float]:
+    base: float = 13.75
+    power: list[float] = [2 ** (i / 12) for i in range(12)]
+    tones: list[float] = [base * (1 << j) * i for j in range(11) for i in power]
+    return tones[3:123]
+
+def linear_interpolation(start: float, end: float, dist: float) -> float:
+    return start * (1 - dist) + end * dist
